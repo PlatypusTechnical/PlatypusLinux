01db6f423b6e99e81d54556bfd02baa2453f5564
Tanglo <lee@platypustechnical.com.au>
2018-10-17 11:00:04 +1100

    Application updates

--- platypusLinux.sh
+++ platypusLinux.sh
@@ -5,24 +5,33 @@ wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key ad
 
 #apt repositories
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+#sudo apt-add-repository ppa:elementary-os/stable
 
 apt-get update
 apt-get upgrade
 
+#tools
+apt-get install curl
+apt-get install software-properties-common
+apt-get install snap
+
 #development tools
 apt-get install vim git gcc g++ doxygen
+curl https://sh.rustup.rs -sSf | sh
+snap install atom
 
 #libraries
 
 #applications
 apt-get install google-chrome-stable
-apt-get install geary 
+apt-get install geary gnome-calendar 
 apt-get install okular libreoffice
 apt-get gimp
 
 #utilities
 apt-get install gparted
 apt-get install cups
+apt-get install gnome-online-accounts
 
 apt autoremove
 apt-get clean
